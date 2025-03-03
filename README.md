c1ba9a932924b7fd8e51297ebf7ea5cd1c05dd8b
aznguy.mp4 <48527495+aznguymp4@users.noreply.github.com>
2025-03-02 19:10:03 -0600

    Preview Spritesheet

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DDR-jackets
 **2025021401**
+
+`generatePreview/index.js` is used to create the following files:
+- `previewSprite.webp`: all jackets scaled down combined into one image; compressed
+- `coordinates.json`: object that contains the coordinates for each jacket
---
+++ generatePreview/index.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const { createCanvas, Image } = require('canvas')
+const webp = require('webp-wasm')
+
+const JACKETS_PATH = "../jacket"
+const jackets = fs.readdirSync(JACKETS_PATH).filter(j=>/(?<!c_.{1,})\.(png|jpg|jpeg|dds)$/.test(j)) // Fitler out courses and any non-image files (such as .DS_Store)
+const GAP = 0
+const JACKET_SIZE = 80
+const WEBP_QUALITY = 30 // quality percentage; int from 0-100
+const SIZEwGAP = (JACKET_SIZE+GAP)
+const SPRITESHEET_WIDTH = SIZEwGAP*Math.ceil(Math.sqrt(jackets.length))
+
+const cvs = createCanvas(SPRITESHEET_WIDTH, SPRITESHEET_WIDTH)
+const ctx = cvs.getContext('2d')
+ctx.fillStyle = '#000'
+ctx.fillRect(0,0,SPRITESHEET_WIDTH,SPRITESHEET_WIDTH)
+
+const coords = {}
+let loaded = 0
+let SPRITESHEET_HEIGHT = 0
+jackets.map((jacketFileName, idx) => {
+  const jacketName = jacketFileName.split('_jk')[0]
+  const posX = (idx*SIZEwGAP) % SPRITESHEET_WIDTH
+  const posY = (~~((idx*SIZEwGAP) / SPRITESHEET_WIDTH)) * SIZEwGAP
+  SPRITESHEET_HEIGHT = Math.max(SPRITESHEET_HEIGHT, posY + JACKET_SIZE)
+
+  coords[jacketName] = [posX,posY]
+  const img = new Image()
+  img.onload = () => {
+    ctx.drawImage(img, posX, posY, JACKET_SIZE, JACKET_SIZE)
+    loaded ++
+    console.log(loaded, 'loaded',jacketFileName)
+
+    if(loaded === jackets.length) {
+      const cropped = createCanvas(SPRITESHEET_WIDTH, SPRITESHEET_HEIGHT)
+      const ctx2 = cropped.getContext('2d')
+      ctx2.drawImage(cvs,0,0)
+      const imgData = ctx2.getImageData(0,0,SPRITESHEET_WIDTH,SPRITESHEET_HEIGHT)
+
+      console.log(`Saving .webp (at ${WEBP_QUALITY}% image quality...)`)
+      fs.writeFileSync('./coordinates.json', JSON.stringify(coords))
+
+      webp.encode(imgData, {
+        quality: WEBP_QUALITY
+      }).then(webpBuffer => {
+        fs.writeFileSync('./previewSprite.webp', Buffer.from(webpBuffer))
+        console.log(`Saved .webp successfully`)
+      }).catch(e => {
+        console.error(`Error saving .webp (${e})`)
+      })
+    }
+  }
+  img.src = `${JACKETS_PATH}/${jacketFileName}`
+})
