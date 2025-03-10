6df08ca7f006918d5f0d811995fc0361e12fe420
aznguy.mp4 <48527495+aznguymp4@users.noreply.github.com>
2025-03-09 19:04:15 -0500

    192px individual previews

---
+++ generatePreview/previewIndividual.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const { createCanvas, Image } = require('canvas')
+const webp = require('webp-wasm')
+
+const JACKETS_PATH = "../jacket"
+const jackets = fs.readdirSync(JACKETS_PATH).filter(j=>!j.startsWith('c_') && /\.(png|jpg|jpeg|dds)$/.test(j)) // Fitler out courses and any non-image files (such as .DS_Store)
+const JACKET_SIZE = 192
+const WEBP_QUALITY = 70
+
+const cvs = createCanvas(JACKET_SIZE, JACKET_SIZE)
+const ctx = cvs.getContext('2d')
+
+jackets.map((jacketFileName, idx) => {
+  const jacketName = jacketFileName.split('_jk')[0]
+  const img = new Image()
+  img.onload = () => {
+    ctx.drawImage(img, 0, 0, JACKET_SIZE, JACKET_SIZE)
+    const imgData = ctx.getImageData(0,0,JACKET_SIZE,JACKET_SIZE)
+
+    webp.encode(imgData, {
+      quality: WEBP_QUALITY
+    }).then(webpBuffer => {
+      fs.writeFileSync(`../preview${JACKET_SIZE}px/${jacketName}.webp`, Buffer.from(webpBuffer))
+      console.log(`Saved ${jacketName}.webp (${WEBP_QUALITY}% quality) successfully`)
+    }).catch(e => {
+      console.error(`Error saving .webp (${e})`)
+    })
+  }
+  img.src = `${JACKETS_PATH}/${jacketFileName}`
+})
